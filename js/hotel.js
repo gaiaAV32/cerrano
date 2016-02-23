1119d1ecde20bb741c37671863d80137eb833468
gaiaAV32 <gaia.digregorio@avenue32.com>
2016-02-23 00:57:23 +0000

    added codepicker

--- js/hotel.js
+++ js/hotel.js
@@ -4,8 +4,23 @@ $(document).ready(function(){
     });
 });
 
-$('#sandbox-container div').datepicker({
-    todayBtn: true,
-    clearBtn: true,
-    daysOfWeekHighlighted: "5,6"
+            // When the document is ready
+
+
+var d = new Date();
+
+var currDate = d.getDate();
+var currMonth = d.getMonth();
+var currYear = d.getFullYear();
+
+var dateStr = currDate + "-" + currMonth + "-" + currYear; 
+console.log(dateStr);
+
+$(document).ready(function () {
+    $('#datepicker').datepicker({
+        format: "dd/mm/yyyy"});
+    
+    $('#datepicker').datepicker("setDate", new Date());
 });
+
+
